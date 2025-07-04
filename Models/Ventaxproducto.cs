7881f72dc61828b122ff5d0905ed32bcc1db26be
LejoMG <156809502+LejoMG@users.noreply.github.com>
2025-07-04 17:14:20 -0500

    Update Ventaxproducto.cs

--- Models/Ventaxproducto.cs
+++ Models/Ventaxproducto.cs
@@ -7,13 +7,13 @@ public partial class Ventaxproducto
 {
     public int IdVentaxproducto { get; set; }
 
-    public int FkVenta { get; set; }
+    public int FkVenta { get; set; } = null
 
-    public int FkProducto { get; set; }
+    public int FkProducto { get; set; } = null
 
     public int Cantidad { get; set; }
 
     public decimal Subtotal { get; set; }
 
-    public virtual Venta FkVentaNavigation { get; set; } = null!;
+    public virtual Venta? FkVentaNavigation { get; set; } = null!;
 }
