cc86cd72b5a9a4b6f942e1cef72021b480d59a95
LejoMG <156809502+LejoMG@users.noreply.github.com>
2025-07-04 18:11:59 -0500

    Update Venta.cs

--- Models/Venta.cs
+++ Models/Venta.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace backend.Models;
-
 public partial class Venta
 {
     public int IdVenta { get; set; }
 
     public DateOnly FechaVenta { get; set; }
 
-    public int FkCliente { get; set; }// ← Corregido: quitado = null!
+    public int? FkCliente { get; set; } // Hacerlo nullable
 
     public string MetodoPago { get; set; } = null!;
 
     public decimal Total { get; set; }
 
-    public virtual Cliente? FkClienteNavigation { get; set; } = null!;
+    public virtual Cliente? FkClienteNavigation { get; set; } // Mantener nullable
 
     public virtual ICollection<Servicioxventum> Servicioxventa { get; set; } = new List<Servicioxventum>();
 
