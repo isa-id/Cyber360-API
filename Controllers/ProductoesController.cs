d1538d6390f1049d04362ffc8b5091adcfa37af8
LejoMG <156809502+LejoMG@users.noreply.github.com>
2025-07-05 16:51:11 -0500

    Update ProductoesController.cs

--- Controllers/ProductoesController.cs
+++ Controllers/ProductoesController.cs
@@ -50,7 +50,7 @@ public async Task<ActionResult> PostProducto([FromBody] ProductoCreateDto dto)
         Nombre = dto.Nombre,
         Cantidad = dto.Cantidad,
         Precio = dto.Precio,
-        FechaCreacion = DateOnly,
+        FechaCreacion = DateOnly.FromDateTime(DateTime.Now)
         FkImagen = dto.FkImagen, // Asignamos solo el ID
         FkCategoria = dto.FkCategoria // Asignamos solo el ID
     };
