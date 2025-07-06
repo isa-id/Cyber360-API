f1e88c379bc24d59f19c6dd4760ea54e0cc29175
LejoMG <156809502+LejoMG@users.noreply.github.com>
2025-07-06 12:34:49 -0500

    Update ServicioxinsumoesController.cs

--- Controllers/ServicioxinsumoesController.cs
+++ Controllers/ServicioxinsumoesController.cs
@@ -1,164 +1,127 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using backend.Models;
 
 namespace backend.Controllers
 {
-    public class ServicioxinsumoesController : Controller
+    [Route("[controller]")]
+    [ApiController]
+    public class ServicioxinsumosController : ControllerBase
     {
         private readonly NeondbContext _context;
 
-        public ServicioxinsumoesController(NeondbContext context)
+        public ServicioxinsumosController(NeondbContext context)
         {
             _context = context;
         }
 
-        // GET: Servicioxinsumoes
-        public async Task<IActionResult> Index()
+        // GET: api/Servicioxinsumos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Servicioxinsumo>>> GetServicioxinsumos()
         {
-            var neondbContext = _context.Servicioxinsumos.Include(s => s.FkProductoNavigation).Include(s => s.FkServicioNavigation);
-            return View(await neondbContext.ToListAsync());
+            return await _context.Servicioxinsumos
+                .Include(s => s.FkProductoNavigation)
+                .Include(s => s.FkServicioNavigation)
+                .ToListAsync();
         }
 
-        // GET: Servicioxinsumoes/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: api/Servicioxinsumos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Servicioxinsumo>> GetServicioxinsumo(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var servicioxinsumo = await _context.Servicioxinsumos
                 .Include(s => s.FkProductoNavigation)
                 .Include(s => s.FkServicioNavigation)
-                .FirstOrDefaultAsync(m => m.IdServicioxinsumo == id);
+                .FirstOrDefaultAsync(s => s.IdServicioxinsumo == id);
+
             if (servicioxinsumo == null)
             {
                 return NotFound();
             }
 
-            return View(servicioxinsumo);
+            return servicioxinsumo;
         }
 
-        // GET: Servicioxinsumoes/Create
-        public IActionResult Create()
-        {
-            ViewData["FkProducto"] = new SelectList(_context.Productos, "IdProducto", "IdProducto");
-            ViewData["FkServicio"] = new SelectList(_context.Servicios, "IdServicio", "IdServicio");
-            return View();
-        }
-
-        // POST: Servicioxinsumoes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: api/Servicioxinsumos
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdServicioxinsumo,FkProducto,FkServicio")] Servicioxinsumo servicioxinsumo)
+        public async Task<ActionResult<Servicioxinsumo>> PostServicioxinsumo(Servicioxinsumo servicioxinsumo)
         {
-            if (ModelState.IsValid)
+            // Validar existencia de las entidades relacionadas
+            if (!await _context.Productos.AnyAsync(p => p.IdProducto == servicioxinsumo.FkProducto))
             {
-                _context.Add(servicioxinsumo);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return BadRequest("El producto especificado no existe");
             }
-            ViewData["FkProducto"] = new SelectList(_context.Productos, "IdProducto", "IdProducto", servicioxinsumo.FkProducto);
-            ViewData["FkServicio"] = new SelectList(_context.Servicios, "IdServicio", "IdServicio", servicioxinsumo.FkServicio);
-            return View(servicioxinsumo);
+
+            if (!await _context.Servicios.AnyAsync(s => s.IdServicio == servicioxinsumo.FkServicio))
+            {
+                return BadRequest("El servicio especificado no existe");
+            }
+
+            _context.Servicioxinsumos.Add(servicioxinsumo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetServicioxinsumo", new { id = servicioxinsumo.IdServicioxinsumo }, servicioxinsumo);
         }
 
-        // GET: Servicioxinsumoes/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var servicioxinsumo = await _context.Servicioxinsumos.FindAsync(id);
-            if (servicioxinsumo == null)
-            {
-                return NotFound();
-            }
-            ViewData["FkProducto"] = new SelectList(_context.Productos, "IdProducto", "IdProducto", servicioxinsumo.FkProducto);
-            ViewData["FkServicio"] = new SelectList(_context.Servicios, "IdServicio", "IdServicio", servicioxinsumo.FkServicio);
-            return View(servicioxinsumo);
-        }
-
-        // POST: Servicioxinsumoes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdServicioxinsumo,FkProducto,FkServicio")] Servicioxinsumo servicioxinsumo)
+        // PUT: api/Servicioxinsumos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutServicioxinsumo(int id, Servicioxinsumo servicioxinsumo)
         {
             if (id != servicioxinsumo.IdServicioxinsumo)
             {
-                return NotFound();
+                return BadRequest();
             }
 
-            if (ModelState.IsValid)
+            // Validar existencia de las entidades relacionadas
+            if (!await _context.Productos.AnyAsync(p => p.IdProducto == servicioxinsumo.FkProducto))
             {
-                try
-                {
-                    _context.Update(servicioxinsumo);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ServicioxinsumoExists(servicioxinsumo.IdServicioxinsumo))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                return BadRequest("El producto especificado no existe");
             }
-            ViewData["FkProducto"] = new SelectList(_context.Productos, "IdProducto", "IdProducto", servicioxinsumo.FkProducto);
-            ViewData["FkServicio"] = new SelectList(_context.Servicios, "IdServicio", "IdServicio", servicioxinsumo.FkServicio);
-            return View(servicioxinsumo);
+
+            if (!await _context.Servicios.AnyAsync(s => s.IdServicio == servicioxinsumo.FkServicio))
+            {
+                return BadRequest("El servicio especificado no existe");
+            }
+
+            _context.Entry(servicioxinsumo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServicioxinsumoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // GET: Servicioxinsumoes/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        // DELETE: api/Servicioxinsumos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteServicioxinsumo(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var servicioxinsumo = await _context.Servicioxinsumos
-                .Include(s => s.FkProductoNavigation)
-                .Include(s => s.FkServicioNavigation)
-                .FirstOrDefaultAsync(m => m.IdServicioxinsumo == id);
+            var servicioxinsumo = await _context.Servicioxinsumos.FindAsync(id);
             if (servicioxinsumo == null)
             {
                 return NotFound();
             }
 
-            return View(servicioxinsumo);
-        }
-
-        // POST: Servicioxinsumoes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var servicioxinsumo = await _context.Servicioxinsumos.FindAsync(id);
-            if (servicioxinsumo != null)
-            {
-                _context.Servicioxinsumos.Remove(servicioxinsumo);
-            }
-
+            _context.Servicioxinsumos.Remove(servicioxinsumo);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return NoContent();
         }
 
         private bool ServicioxinsumoExists(int id)
